5f2918fe4cc24e520038f45cba227173dca9e0a8
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-10-16 18:41:09 +0530

    Add files via upload

---
+++ Codechef/Lucky_Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;cin>>t;
+    while (t--)
+    {
+        int n1,n2,n3;
+        cin>>n1>>n2>>n3;
+        if(n1==7||n2==7||n3==7){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}
