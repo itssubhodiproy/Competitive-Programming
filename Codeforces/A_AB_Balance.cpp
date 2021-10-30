53e194227faaa333107d4e155c4fdaf4130fd71a
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-10-30 10:21:37 +0530

    Add files via upload

---
+++ Codeforces/A_AB_Balance.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < s.length() - 1; i++)
+        {
+            if (s[i] == 'a' && s[i + 1] == 'b')
+            {
+                count1++;
+            }
+            if (s[i] == 'b' && s[i + 1] == 'a')
+            {
+                count2++;
+            }
+        }
+
+        if (count1 != count2)
+        {
+            if (count1 > count2)
+            {
+				s[0]='b';
+			}
+            if (count2 > count1)
+            {
+				s[0]='a';	
+        }
+		cout<<s<<endl;
+		}
+        else
+        {
+            cout << s << endl;
+        }
+        
+    }
+
+    return 0;
+}
