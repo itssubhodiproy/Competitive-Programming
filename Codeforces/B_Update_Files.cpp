d1b701c7b86e243282b4e509fbca2b6536b065a6
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-10-31 10:27:17 +0530

    Add files via upload

---
+++ Codeforces/B_Update_Files.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define  i long long int
+#define  w (a,b) while (a > b)
+int main()
+{
+    i t;
+    cin >> t;
+    while (t--)
+    {
+        i n, k;
+        cin >> n >> k;
+        i count = 0;
+        i ans = 1;
+        while (n > ans)
+        {
+            ans += min(ans, k);
+            count++;
+            // i++;
+        }
+        cout << count << endl;
+    }
+
+    return 0;
+}
