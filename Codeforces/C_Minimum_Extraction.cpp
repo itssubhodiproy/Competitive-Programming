3c0276314183a3affc3f2b28a11262cddcbade7d
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-11-03 21:26:18 +0530

    Add files via upload

---
+++ Codeforces/C_Minimum_Extraction.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;cin>>t;
+while (t--)
+{
+    int n;cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    if (n==1)
+    {
+        cout<<arr[0]<<endl;
+        continue;
+    }
+     if (n==2)
+    {
+        cout<<min(arr[0],arr[1])<<endl;
+        continue;
+    } 
+  
+    sort(arr,arr+n);
+
+    int maxi = INT_MIN;
+    int diff;
+    for (int i = n-1; i > 0; i--)
+    {
+        diff=arr[i]-arr[i-1];
+        maxi=max(maxi,diff);
+    }
+    cout<<maxi<<endl;
+    
+}
+
+}
