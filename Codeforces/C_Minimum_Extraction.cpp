277842079efa408e3b0dcda46fbc4019e8c95de6
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-11-04 08:07:29 +0530

    Update C_Minimum_Extraction.cpp

--- Codeforces/C_Minimum_Extraction.cpp
+++ Codeforces/C_Minimum_Extraction.cpp
@@ -1,39 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-//                                                         <<<---Have some edgecases---->>>>
 
-int main(){
-    int t;cin>>t;
-while (t--)
+int main()
 {
-    int n;cin>>n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        cin>>arr[i];
+        int n;
+        cin >> n;
+        int mini = INT_MAX;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        if (n == 1)
+        {
+            cout << arr[0] << endl;
+            continue;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            mini = min(arr[i], mini);
+        }
+        sort(arr, arr + n);
+        int maxi = mini;
+        for (int i = 0; i < n - 1; i++)
+        {
+            mini = max(mini, arr[i + 1] - arr[i]);
+        }
+        cout << mini << endl;
     }
-    if (n==1)
-    {
-        cout<<arr[0]<<endl;
-        continue;
-    }
-     if (n==2)
-    {
-        cout<<min(arr[0],arr[1])<<endl;
-        continue;
-    } 
-  
-    sort(arr,arr+n);
-
-    int maxi = INT_MIN;
-    int diff;
-    for (int i = n-1; i > 0; i--)
-    {
-        diff=arr[i]-arr[i-1];
-        maxi=max(maxi,diff);
-    }
-    cout<<maxi<<endl;
-    
-}
-
 }
