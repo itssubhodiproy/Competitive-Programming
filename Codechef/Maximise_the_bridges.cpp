82dd13e310f908c84dfe7586fc3bc94665ee7c63
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-11-12 12:32:34 +0530

    Update Maximise_the_bridges.cpp

--- Codechef/Maximise_the_bridges.cpp
+++ Codechef/Maximise_the_bridges.cpp
@@ -1,75 +1,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-                                                //Getting edgecases//
+
+
 int main()
 {
-    ll t;cin>>t;
+    ll t;
+    cin >> t;
     while (t--)
     {
-        ll n,m;cin>>n>>m;
-        
-        if (n==m)
+        ll n, m;
+        cin >> n >> m;
+
+        if (n == m)
         {
-            if(m==(n*(n-1)/2)){
+            if (m == (n * (n - 1) / 2))
+            {
                 for (ll i = 1; i <= m; i++)
                 {
-                    if (i==m)
+                    if (i == m)
                     {
-                        cout<<i<<" "<<'1'<<endl;
+                        cout << i << " " << '1' << endl;
                         continue;
                     }
-                    
-                    cout<<i<<" "<<i+1<<endl;
+
+                    cout << i << " " << i + 1 << endl;
                 }
-                
             }
-            else{
+            else
+            {
                 for (ll i = 1; i <= m; i++)
                 {
-                    if (i==m)
+                    if (i == m)
                     {
-                        cout<<'1'<<" "<<'3'<<endl;
+                        cout << '1' << " " << '3' << endl;
                         continue;
-                    }  
-                    cout<<i<<" "<<i+1<<endl;
+                    }
+                    cout << i << " " << i + 1 << endl;
                 }
-                
             }
-            
         }
-        else if(m==n-1)
+        else if (m == n - 1)
         {
             for (ll i = 1; i <= m; i++)
             {
-                cout<<i<<" "<<i+1<<endl;  
+                cout << i << " " << i + 1 << endl;
             }
         }
-        else if(m>n)
+        else if (m > n)
         {
-            // for (ll i = 1; i < n; i++)
+
+            // int j=1;
+            // int ans=2;
+            // while (m--)
             // {
-            //     cout<<i<<" "<<i+1<<endl;  
+            //     if(ans==n+1){
+            //         j++;
+            //         ans=j+1;
+            //     }
+            //     cout<<j<<" "<<ans<<endl;
+            //     ans++;
             // }
-            int j=1;
-            int ans=2;
+            int first = 1;
+            int diff = 1;
+            int temp;
+            int prevfirst = first;
             while (m--)
             {
-                // if(k+1==n){
-                //     a++;k=3;
-                // }
-                if(ans==n+1){
-                    j++;
-                    ans=j+1;
+                if (first + diff > n)
+                {
+                    diff++;
+                    first = 1;
                 }
-                cout<<j<<" "<<ans<<endl;
-                ans++;  
-            }
-            
-        }
-        
-        
 
+                temp = first + diff;
+                prevfirst = min(first, prevfirst);
+                                                            if (prevfirst > 1 && diff > 1)
+                                                            {
+
+                                                                prevfirst--;
+                                                                cout << prevfirst << " " << temp << endl;
+                                                                continue;
+                                                            }
+                cout << first << " " << temp << endl;
+                first++;
+                prevfirst = first;
+            }
+        }
     }
     return 0;
 }
