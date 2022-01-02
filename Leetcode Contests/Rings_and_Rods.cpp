55a479a7394fbb5398bf123cee8548a5d9aacc33
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-01-02 10:03:44 +0530

    Update Rings_and_Rods.cpp

--- Leetcode Contests/Rings_and_Rods.cpp
+++ Leetcode Contests/Rings_and_Rods.cpp
@@ -1,42 +1,27 @@
 int countPoints(string rings)
-{
-	set<string> s;
+{	set<string> s;
 	string elem = "";
 	for (int i = 0; i < rings.length(); i += 2)
 	{
-		elem = "";
-		elem = rings[i];
-		elem += rings[i + 1];
-		s.insert(elem);
+	elem = "";
+	elem = rings[i];
+	elem += rings[i + 1];
+	s.insert(elem);
 	}
-	// set<int>::iterator itr;
 	vector<string> p(10, "");
 	int count = 0;
-	for (auto it : s)
+	for (auto it : s){
+	 if (p[it[1]-'0'] != "")
 	{
-		// cout<<it[0]<<" ";
-		// cout<<it[1]<<"\n";
-		// p[it[1]].push_back(it[0]);
-		if (p[it[1]-'0'] != "")
-		{
-			// p[it[1]]+=it[0];
-			p[it[1]-'0'].push_back(it[0]);
-			// cout<<p[it[1]]<<" ";
-			continue;
-		}
-		p[it[1]-'0'] = it[0];
-		// cout<<p[it[1]]<<" ";
-		// cout<<p[it[0]];
-		// += it[i];
-		// p[it[i + 1]].push_back(it[i]);
+	p[it[1]-'0'].push_back(it[0]);
+	continue;
+	}
+	p[it[1]-'0'] = it[0];
 	}
 
 	for(auto it: p){
-		if (it.length()==3){
-			count++;
+		if (it.length()==3){ count++;
 		}
-		// cout<<it<<" \n";
 	}
-
 	return count;
 }
