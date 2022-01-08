f762d1eea69749016f0103b2b7ae7834937de94b
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-01-08 20:05:07 +0530

    Add files via upload

---
+++ Random contest/Codeforces Gym/B_Little_Frog.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+
+    ll n;
+    cin >> n;
+    // ll arr[n];
+    queue<ll> v;
+    // vector<ll> v;
+    ll i = 1;
+    ll last = n;
+    
+    while (v.size() != n)
+    {
+        if ( (n%2!=0) && (v.size() + 1 == n))
+        {
+            v.push(i);
+            continue;
+        }
+        v.push(i);
+        i++;
+        v.push(last);
+        last--;
+    }
+    while (!v.empty())
+    {
+        cout << v.front() << " ";
+        v.pop();
+    }
+    // for (auto it : v)
+    // {
+    //     cout<<it<<" ";
+    // }
+
+    cout << "\n";
+}
