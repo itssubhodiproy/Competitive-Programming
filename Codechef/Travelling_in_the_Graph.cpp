812e52dd898b658c37a06f104010630ba70d9abe
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-03-03 10:02:09 +0530

    Add files via upload

--- Codechef/Travelling_in_the_Graph.cpp
+++ Codechef/Travelling_in_the_Graph.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-void dfs(int node, vector<int> &vis, vector<int> adj[])
-{
-    vis[node] = 1;
-    for (auto it : adj[node])
-    {
-        if (!vis[it])
-        {
-            dfs(it, vis, adj);
-        }
-    }
-}
+
 
 int main()
 {
@@ -18,47 +8,65 @@ int main()
     cin >> tc;
     while (tc--)
     {
-        int V, E;
-        cin >> V >> E;
-        unordered_set<int> s;
-        vector<int> adj[V];
-        int count;
-        if (E < V)
+        int n, m, source;
+        cin >> n >> m;
+        vector<pair<int, int>> g[n + 1]; // 1-indexed adjacency list for of graph
+
+        int a, b, wt;
+        vector<int> vis(n + 1, 0);
+        for (int i = 0; i < m; i++)
         {
-            count = V - E;
+            cin >> a >> b;
+            a--;
+            b--;
+            g[a].push_back(make_pair(b, 0));
+            g[b].push_back(make_pair(a, 0));
+            if (abs(b - a) == 1)
+            {
+                vis[min(a, b)] = 1;
+            }
         }
-        for (int i = 1; i <= E; i++)
+        for (int i = 0; i < n - 1; ++i)
         {
-            int u, v;
-            cin >> u >> v;
-            s.insert(u);
-            s.insert(v);
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            if (!vis[i])
+            {
+                vis[i] = 1;
+                g[i].push_back(make_pair(i + 1, 1));
+                g[i + 1].push_back(make_pair(i, 1));
+            }
         }
+        
+
+        // Dijkstra's algorithm begins from here
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // min-heap ; In pair => (dist,from)
+        vector<int> distTo(n + 1, INT_MAX);                                                 // 1-indexed array for calculating shortest paths;
+
+        distTo[0] = 0;
+        pq.push(make_pair(0, 0)); // (dist,from)
+
+        while (!pq.empty())
+        {
+            int dist = pq.top().first;
+            int prev = pq.top().second;
+            pq.pop();
+
+            vector<pair<int, int>>::iterator it;
+            for (it = g[prev].begin(); it != g[prev].end(); it++)
+            {
+                int next = it->first;
+                int nextDist = it->second;
+                if (distTo[next] > distTo[prev] + nextDist)
+                {
+                    distTo[next] = distTo[prev] + nextDist;
+                    pq.push(make_pair(distTo[next], next));
+                }
+            }
+        }
+
         // string s1;
         // cin>>s1;
 
-        vector<int> vis(V + 1, 0);
-        for (int i = 1; i <= V; i++)
-        {
-            if ((s.count(i) == 0))
-            {
-                continue;
-            }
-            else if (s.count(i) == 1 && !vis[i])
-            {
-                dfs(i, vis, adj);
-                count++;
-            }
-        }
-        cout << count << "\n";
-
-        // for (int i = 0; i < ans.size(); i++)
-        // {
-        //     cout << ans[i] << " ";
-        // }
-        // cout << endl;
+        cout << distTo[n-1] << "\n";
     }
     return 0;
 }
