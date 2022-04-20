b80f040eab27cbfa5d37f2ffa3789749577a397b
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-04-20 23:17:07 +0530

    Add files via upload

---
+++ Codechef/Dark_Light.cpp
@@ -0,0 +1,103 @@
+/*Boil
+NAME: SUBHODIP ROY
+BRANCH: COMPUTER SCIENCE AND ENGINEERING
+COLLEGE: NETAJI SUBHASH ENGINEERING COLLEGE
+GITHUB: subh-23
+CODEFORCES: subh_23
+CODECHEF: subh_23
+GRADUATION YEAR: 2024
+*/
+// Header Files and Macros//------------------------------------------------------------------>>
+#pragma GCC optimize("O3,unroll-loops")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace chrono;
+using namespace __gnu_pbds;
+#define int long long int
+#define float double
+#define all(x) (x).begin(), (x).end()
+#define vi vector<int>
+#define mod (int)1e9
+#define printCase(x) cout << "Case #" << x << ": "
+// GNU link : https://goo.gl/WVDL6g
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+
+// End Here//--------------------------------------------------------------------------------->>
+void readVector(vi &v, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+}
+void Print(int n)
+{
+    cout << n << '\n';
+}
+
+// put function
+
+// Code Here --------------------------------------------------------------------------------->>
+void solve(int z)
+{
+    // Input//
+    int n, k;
+    cin >> n >> k;
+    // printCase(z); //(For Google Competitions)
+    // Code it Here
+    if (k == 1)
+    {
+        if (n == 0 || n % 4 == 0)
+        {
+            cout << "On" << '\n';
+        }
+        else
+        {
+            cout << "Ambiguous" << '\n';
+        }
+    }
+    else
+    {
+        if (n % 4 == 0)
+        {
+            cout << "Off" << '\n';
+        }
+        else
+        {
+            cout << "On" << '\n';
+        }
+
+        // cout << "Ambiguous" << '\n';
+    }
+}
+//-------------------------------------------------------------------------------------------->>
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int google = 1;
+    cin >> google; // if not multiple testcase, remove it
+    for (int i = 1; i <= google; i++)
+    {
+        solve(i);
+    }
+    return 0;
+}
+/*
+INSTRUCTIONS TO KEEP IN MIND
+-> Read the problem statement very clearly.
+-> Dig deep into the problem statement and try to find the crispy part
+   of the question (Basically Logic).
+-> Find out how the crispy part is helping you to design the algorithm
+   to solve the current question
+-> Try to find the edgecases (Smaller & random cases).
+-> Formulate the code in paper.
+-> Code it.. Woohhhaaaahhh!! (Accepted)
+*/
